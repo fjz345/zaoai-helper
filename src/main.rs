6fd3737e7cf0c6fb0ba77d9c761c110ebf83e9fd
FjZ345 <fjz345@hotmail.com>
2025-07-09 10:15:11 +0200

    main cleanup

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,7 @@ use crate::chapters::{extract_chapters, parse_chapter_xml, read_chapters_from_mk
 mod chapters;
 
 fn main() {
-    let xml_file = "chapters.xml";
-
     let mkv_file = "test/test1.mkv";
-    extract_chapters(mkv_file, xml_file).expect("ADS");
-
-    let xml_content = fs::read_to_string(xml_file).expect("FAUILED");
-
-    let parsed = parse_chapter_xml(&xml_content).expect("FAISD");
-
     let result = read_chapters_from_mkv(mkv_file);
     result.expect("failed");
 }
