43bedf2dc58aa184dc9df4bb8ba78d4a64e97c9d
FjZ345 <fjz345@hotmail.com>
2025-07-25 15:09:40 +0200

    using dotenvy

--- src/main.rs
+++ src/main.rs
@@ -23,33 +23,31 @@ Want to do:
 */
 
 fn main() -> Result<()> {
+    dotenvy::dotenv().ok();
     unsafe { env::set_var("RUST_BACKTRACE", "1") };
 
     #[allow(unused)]
     let args: Vec<String> = env::args().collect();
-
     // // args[0] is the program name
     // if args.len() < 2 {
     //     eprintln!("Usage: {} <path>", args[0]);
     // }
 
-    let path = r#"\\Znas\plex\Series"#;
-    let out_path = "output\\list_dir_split.json";
-    // let path = "test\\test_Source";
-    // let out_path = "output\\list_dir_split.json";
-    path_exists(path);
+    let media_path =
+        std::env::var("ZAOAI_MEDIA_PATH").unwrap_or_else(|_| "test/test_Source".to_string());
+    let list_dir_split_out_path = "output\\list_dir_split.json";
+    path_exists(&media_path);
 
-    // println!("Collecting list_dir_split...");
-    // collect_list_dir_split(path, out_path).unwrap();
-    // println!("Finished collecting list_dir_split!");
+    println!("Collecting list_dir_split...");
+    collect_list_dir_split(media_path, list_dir_split_out_path).unwrap();
+    println!("Finished collecting list_dir_split!");
 
     println!("Collecting zaoai_labels...");
-    let out_path = "output\\zaoai_labels";
+    let zaoai_labels_out_path = "output\\zaoai_labels";
     // let out_path = "output\\zaoai_labels";
     let read_list_dir_split =
         ListDirSplit::from_file_json("output\\list_dir_split_001.json").unwrap();
-
-    collect_zaoai_labels(&read_list_dir_split, out_path)?;
+    collect_zaoai_labels(&read_list_dir_split, zaoai_labels_out_path)?;
     println!("Finished collecting zaoai_labels!");
 
     Ok(())
